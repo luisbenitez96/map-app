dd04b4cde1684ce48ad211ce18344eb5e97807f3
Luis Benitez <luisbenitez544@gmail.com>
2025-06-04 12:41:47 -0500

    ready to deply

--- src/context/places/PlacesContext.ts
+++ src/context/places/PlacesContext.ts
@@ -3,7 +3,6 @@ import { createContext } from "react";
 export interface PlacesContextProps {
   isLoading: boolean;
   userLocation?: [number, number];
-  searchPlacesByTerm: (query: string) => Promise<any>;
 }
 
 export const PlacesContext = createContext<PlacesContextProps>(
