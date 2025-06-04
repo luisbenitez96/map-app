afa9080677625d08bf8f0a50857c18a3bf4cd473
Luis Benitez <luisbenitez544@gmail.com>
2025-06-04 12:32:06 -0500

    remove key

--- src/apis/searchApi.ts
+++ src/apis/searchApi.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const searchApi = axios.create({
   baseURL: "https://nominatim.openstreetmap.org",
   params: {
-    key: "AIzaSyB7KxfnKdO_jiGKFZALH_LAm0nwZUUuoMk",
     format: "geojson",
     polygon_geojson: 1,
     addressdetails: 1,
